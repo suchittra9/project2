d43240c0b018854626be0eeb272febe44cbe39d0
Suchittra Muangman <suchittra.twopee@gmail.com>
2020-08-16 13:15:33 +0700

    update

--- database/migrations/2020_08_16_061050_create_categories_table.php
+++ database/migrations/2020_08_16_061050_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCatrgoriesTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateCatrgoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('catrgories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string("name");
             $table->timestamps();
+
         });
     }
 
@@ -27,6 +28,6 @@ class CreateCatrgoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('catrgories');
+        Schema::dropIfExists('categories');
     }
 }
